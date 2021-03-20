163affc2a4fc5fde5355465dcfb6209c26f2b528
Jorge Ossa <jorgehossa@gmail.com>
2021-03-19 23:33:10 -0500

    📦 NEW: Add about and projects pages

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Avatar from '../components/Avatar';
+
+const About = () => (
+  <Layout>
+    <Avatar size='150'/>
+    <h1>Jorge Ossa</h1>
+    <h3>Frontend Developer</h3>
+    <p>
+    A passionate designer and web developer, solving problems, and detail attention oriented.
+    </p>
+
+  </Layout>
+)
+
+export default About;
---
+++ pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const Projects = () => (
+  <Layout>
+    <h1>Projects</h1>
+  </Layout>
+)
+
+export default Projects;
